bc97804b916c1cbefbb8b039b1b323e9af744c54
HalfBurntToast <halfburnttoast@gmail.com>
2022-09-08 12:07:06 -0400

    Forgot to bump version number for last commit

--- dungeon_cross.py
+++ dungeon_cross.py
@@ -33,7 +33,7 @@ import sound_handler
 from map_object_enum import MapObject
 from resource_path import resource_path
 
-VERSION = "v0.14.7"
+VERSION = "v0.14.8"
 
 TILE_SIZE = 90
 G_RESOLUTION = (TILE_SIZE * 9, TILE_SIZE * 9)
